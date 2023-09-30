2195688044c5d7a4ed152a125fefa6808f438dcd
thahirimran <106942239+thahirimran@users.noreply.github.com>
2023-09-30 19:48:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [num1,setNum1] = useState("");
+  const [num2,setNum2] = useState("");
+  const [result,setResult] = useState("");
+
+  function handleAdd(){
+      const sum = Number(num1) + Number(num2);
+      setResult(sum);
+  }
+
+  function handleSub(){
+      const sum = Number(num1) - Number(num2);
+      setResult(sum);
+  }
+
+  function handleMul(){
+      const sum = Number(num1) * Number(num2);
+      setResult(sum);
+  }
+
+  function handleDiv(){
+      const sum = Number(num1) / Number(num2);
+      setResult(sum);
+  }
+
+  function handleClear(){
+
+    setNum1("");
+    setNum2("");
+    setResult("");
+      
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <label className='label'>Num1 :</label>
+        <input type="number" value={num1} className='input' placeholder='Enter Number 1' 
+        onChange={function(event){
+            setNum1(event.target.value)
+        }}/>  <br></br> <br></br>
+
+        <label className='label'>Num2 :</label>
+        <input type="number" className='input' value={num2} placeholder='Enter Number 2' 
+        onChange={function(event){
+            setNum2(event.target.value)
+        }}/>  <br></br> <br></br>
+
+        <button className='button' onClick={handleAdd}>+</button> &nbsp;
+        <button className='button' onClick={handleSub}>-</button> &nbsp;
+        <button className='button' onClick={handleMul}>*</button> &nbsp;
+        <button className='button' onClick={handleDiv}>/</button> &nbsp;
+
+        <br></br> <br></br>
+
+        <label className='label'>Ans :</label>
+        <input className='input'  value={result} />
+
+        <br></br> <br></br>
+
+        <button className='button' onClick={handleClear}>Clear</button>
+      
     </div>
   );
 }
